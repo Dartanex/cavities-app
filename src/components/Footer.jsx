691621f220652cd18135ae6e6e44d7dd0818a839
Josue Orantes <daniel02josue@gmail.com>
2024-05-21 05:31:04 -0600

    :lipstick:style(components):modify style in footer and dropdownbutton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export const Footer = () => {
     <>
         <footer className='bottom-0 mt-5 bg-white/30 p-2'>
             <div className='w-1/2 px-2 md:ms-16 flex'>
-                <img loading='lazy' className='w-40 rounded-full' src={logodentart} alt="logo Dentart" />
-                <p className=' font-bold text-2xl md:text-3xl mt-16'>DENTART</p>
+                <img loading='lazy' className='w-36 md:w-40 rounded-full' src={logodentart} alt="logo Dentart" />
+                <p className=' font-bold text-xl sm:text-2xl md:text-3xl mt-16'>DENTART</p>
             </div>
             <div className='mt-2'>
                 <p className='text-slate-200 text-center mb-2'>
