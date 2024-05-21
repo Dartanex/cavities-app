f0408ecc2c424a54b3a1320ef0fed4b8de39e1e0
Josue Orantes <daniel02josue@gmail.com>
2024-05-21 05:19:31 -0600

    :lipstick:style(components):modify style in card components and footer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,15 +7,15 @@ export const Card = (props) => {
     return(
         <>
             <div className="rounded-xl shadow-lg backdrop-blur-md bg-black/40 container mt-6 md:my-2 mx-auto w-8/12 md:w-9/2">
-                <div className="p-5 flex flex-col content-between gap-4">
+                <div className="p-4 flex flex-col content-between gap-4">
                     <div className="rounded-xl overflow-hidden">
                         <img loading='lazy' src={cavities} alt="Caries dental" />
                     </div>
-                    <h3 className="text-2xl md:text-3xl font-semibold text-white">{props.title}</h3>
+                    <h3 className="text-xl md:text-3xl font-semibold text-white">{props.title}</h3>
                     <div>
                         <p className='font-base text-white'>{props.description}</p>
                     </div>
-                    <div className='mt-4 md:mt-10'>
+                    <div className=' md:mt-10'>
                         <Link to={props.to} className='rounded-md p-2 bg-violet-500 font-semibold text-slate-100'>{props.button}</Link>
                     </div>
                 </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom'
 import logodentart from '../assets/dentart-logo.png'
 
 export const Footer = () => {
   return (
     <>
         <footer className='bottom-0 mt-5 bg-white/30 p-2'>
-            <div className='w-1/2 ms-5 md:ms-16 flex'>
+            <div className='w-1/2 px-2 md:ms-16 flex'>
                 <img loading='lazy' className='w-40 rounded-full' src={logodentart} alt="logo Dentart" />
-                <p className=' font-bold text-3xl mt-16'>DENTART</p>
+                <p className=' font-bold text-2xl md:text-3xl mt-16'>DENTART</p>
             </div>
             <div className='mt-2'>
                 <p className='text-slate-200 text-center mb-2'>
